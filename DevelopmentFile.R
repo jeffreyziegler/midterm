f24ad3e98746db55446ed92a7196ad35d3f0cf07
zieglerjef <zieglerjef@gmail.com>
2016-03-23 20:34:00 -0500

    Finish DevelopmentFile.R

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -36,14 +36,39 @@ build(current.code, path=getwd())
 library(integrateItPack)
 
 ### test functionality ###
-xTest <- as.numeric(c(1:10))
-yTest <- rep(1, 10)
-integrateIt(xVec = xTest, yVec = yTest, a = 1, b = 10, rule ="Simpson")
-testTrap <- integrateIt(xVec = xTest, yVec = yTest, a = 1, b = 10, rule ="Trapezoid")
-print(testTrap)
-plot(testTrap)
+# create test data
+xTest1 <- as.numeric(c(1:10))
+yTest1 <- cos(xTest2)
+# create object of class Simpson and use integrateIt function
+integrateIt(xVec = xTest1, yVec = yTest1, a = 1, b = 10, rule ="Simpson")
 
-#normal distribution
+### test plot and print Trapezoid ###
+# assign object of class Trapezoid and use integrateIt function
+testTrap1 <- integrateIt(xVec = xTest1, yVec = yTest1, a = 1, b = 10, rule ="Trapezoid")
+# test functions 
+# show all attributes
+show(testTrap1)
+# print (just show result)
+print(testTrap1)
+# plot
+plot(testTrap1)
+
+### test plot and print Simpson ###
+# assign object of class Simpson and use integrateIt function
+xTest2 <- as.numeric(c(1:15))
+yTest2 <- cos(xTest2)
+testSimp1 <- integrateIt(xVec = xTest2, yVec = yTest2, a = 1, b = 15, rule ="Simpson")
+# test functions 
+# show
+show(testSimp1)
+# print
+print(testSimp1)
+# plot
+plot(testSimp1)
+
+
+### test tolTest ###
+# normal distribution
 phi <- function(x) exp(-x^2/2)/sqrt(2*pi)
 tolTest(inputFunc = phi, a = 0, b = 1, tol=1e-8, rule="Trapezoid")
 tolTest(inputFunc = phi, a = 0, b = 1, tol=1e-8, rule="Simpson")
